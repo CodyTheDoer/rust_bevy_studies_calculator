937bf2571af98881d6753602990a08134646e3e8
CodyTheDoer <me@codythedoer.com>
2024-10-06 22:34:34 -0400

    Making progress on setup, I have back end built and I whipped together a model in blender. Imported via copying an example for 3D and adjusted the camera, will look into importing a better setup. Also need to figure out diffuse and specular maps.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,122 @@
+use bevy::{
+    pbr::{CascadeShadowConfigBuilder, DirectionalLightShadowMap},
+    prelude::*,
+};
+use std::f32::consts::*;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(20.0, 20.0, 35.0)
+                .looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
+            ..default()
+        },
+        EnvironmentMapLight {
+            diffuse_map: asset_server.load("environment_maps/pisa_diffuse_rgb9e5_zstd.ktx2"),
+            specular_map: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
+            intensity: 250.0,
+        },
+    ));
+
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        // This is a relatively small scene, so use tighter shadow
+        // cascade bounds than the default for better quality.
+        // We also adjusted the shadow map to be larger since we're
+        // only using a single cascade.
+        cascade_shadow_config: CascadeShadowConfigBuilder {
+            num_cascades: 1,
+            maximum_distance: 1.6,
+            ..default()
+        }
+        .into(),
+        ..default()
+    });
+    commands.spawn(SceneBundle {
+        scene: asset_server
+        .load(GltfAssetLabel::Scene(0).from_asset("Calculator.glb")),
+        ..default()
+    });
+}
+
+fn animate_light_direction(
+    time: Res<Time>,
+    mut query: Query<&mut Transform, With<DirectionalLight>>,
+) {
+    for mut transform in &mut query {
+        transform.rotation = Quat::from_euler(
+            EulerRot::ZYX,
+            0.0,
+            time.elapsed_seconds() * PI / 5.0,
+            -FRAC_PI_4,
+        );
+    }
+}
+
+pub struct HelloPlugin;
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, add_user);
+        app.add_systems(Startup, add);
+        app.add_systems(Startup, subtract);
+        app.add_systems(Startup, divide);
+        app.add_systems(Startup, multiply);
+        app.add_systems(Update, greet_people);
+    }
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add() {
+    let result = bevy_calculator::add(24, 49);
+    println!("add result: {:?}", result)
+}
+
+fn subtract() {
+    let result = bevy_calculator::subtract(24, 49);
+    println!("subtract result: {:?}", result)
+}
+
+fn divide() {
+    let result = bevy_calculator::divide(24, 49);
+    println!("divide result: {:?}", result)
+}
+
+fn multiply() {
+    let result = bevy_calculator::multiply(24, 49);
+    println!("multiply result: {:?}", result)
+}
+
+fn add_user(mut commands: Commands) {
+    commands.spawn((Person, Name("User".to_string())));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
 fn main() {
-    let result = bevy_calculator::divide(24, 2);
-    println!("result: {:?}", result)
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(HelloPlugin)
+        .insert_resource(DirectionalLightShadowMap { size: 4096 })
+        .add_systems(Startup, setup)
+        .add_systems(Update, animate_light_direction)
+        .run();
 }
